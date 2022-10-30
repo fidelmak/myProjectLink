76858c66104569f7306240b7d32e4087a57e68d2
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-10-30 08:54:55 +0100

    Reduced height

--- mylink/src/components/Button.jsx
+++ mylink/src/components/Button.jsx
@@ -5,7 +5,7 @@ function Button({name}) {
     return (
         <div>
             <div className="flex justify-center">
-            <button type="button " className=" border-2 border-white bg-pink-400 mb-2 rounded-full p-5 w-2/3 md:w-2/4 mt-12 hover:shadow-lg transform hover:scale-125 hover:font-bold hover:bg-black  hover:border-b border-white  hover:text-white transition ease-out duration-300" >
+            <button type="button " className=" border-2 border-white bg-gray-300 mb-2  p-2 w-2/3 md:w-2/4 mt-6 hover:shadow-lg transform hover:scale-125 hover:font-bold hover:bg-black  hover:border-b border-white  hover:text-white transition ease-out duration-300" >
             <img src={img} alt="" className="  h-5 rounded-full w-5 mr-2 inline-block" /> 
             <a href="https://twitter.com/DelisIgib">{name}</a> 
             </button>
