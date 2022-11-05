c337b5127bd1b69dff3df37b617f9ec25a8acafb
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-11-05 06:14:22 +0100

    added- new link added - contact me button

---
+++ mylink/src/components/Contact.jsx
@@ -0,0 +1,43 @@
+import React, { useState} from 'react'
+
+
+
+function Contact(){
+    const[name, setName]= useState('')
+    const[country, setCountry] =useState('')
+    const[age, setAge] =useState()
+    const handleClick=()=>{
+        var User={
+            name:'name',
+            country:'country',
+            age:'age'
+        }
+       console.log(User)
+        
+    }
+    return <div className='formz'>
+        <h2>Form submission</h2>
+        <form className='iformz' onClick={handleClick}>
+        <label className='ilabel'>Name</label>
+        <br />
+        <input className='in' type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+        <br />
+        <label  className='ilabel'>Age</label>
+        <br />
+        <input className='in'type="text" value={age} onChange={(e)=>setAge(e.target.value) }/>
+        <br />
+        <label className='ilabel'>Country</label>
+        <br />
+        
+        <input className='in' type="text" value={country} onChange={(e)=>setCountry(e.target.value)}/>
+        <br />
+        <br />
+        
+        
+        </form>
+        
+        <button onClick={handleClick} className="btn">submit</button>
+        
+    </div>
+}
+export default Contact
