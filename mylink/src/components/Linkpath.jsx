13bedb50dc72494fcaf08c8d9a7b47c3eaa1274e
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-11-05 11:06:04 +0100

    Update Linkpath.jsx

--- mylink/src/components/Linkpath.jsx
+++ mylink/src/components/Linkpath.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Contact from './Contact'
-import {Link} from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 import Button from './Button';
 
 
@@ -9,9 +9,9 @@ function Linkpath() {
     <div>
         
                     
-       <Link to="/Contact">
+       
  <Button name="Contact me" link="/Contact" ></button>
-</Link>
+
         
                     
                     
