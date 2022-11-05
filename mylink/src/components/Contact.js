cbef6d9411feec20d532a1357f8d03c98730d5da
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-11-05 23:49:47 +0100

    fix: A bug fix

--- mylink/src/components/Contact.js
+++ mylink/src/components/Contact.js
@@ -4,14 +4,14 @@ import Footer from './Footer'
 
 
 function Contact(){
-    const[name, setName]= useState('')
-    const[country, setCountry] =useState('')
-    const[age, setAge] =useState()
+    const[firstname, setFirstname]= useState('')
+    const[lastname, setLastname] =useState('')
+    const[email, setemail] =useState()
     const handleClick=()=>{
         var User={
-            name:'name',
-            country:'country',
-            age:'age'
+            firstname:'firstname',
+            lastname:'lastname',
+            email:'email'
         }
        console.log(User)
         
@@ -27,17 +27,17 @@ function Contact(){
       <form action="#" className="space-y-8">
           <div className='grid gap-4 grid-cols-2'>
                 <div className='w-full '>
-              <label for="Firstname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First name</label>
+              <label id='first_name' for="Firstname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First name</label>
               <input type="name" id="name" className="shadow-sm py-3 px-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Enter your first name" required />
               </div>
               <div className='w-full py-1'>
-              <label for="LastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last name</label>
+              <label id='last_name' for="LastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last name</label>
               <input type="LastName" id="email" className="shadow-sm  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Enter your last name" required />
               </div>
           </div>
           <div>
-              <label for="Email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-              <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="yourname@gmail.com" required />
+              <label id='email' for="Email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+              <input type="text" id="message" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="yourname@gmail.com" required />
           </div>
           <div className="sm:col-span-2">
               <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
@@ -48,7 +48,7 @@ function Contact(){
     
     <label for="unchecked-checkbox" class="ml-2 text-sm font-medium text-gray-500 dark:text-gray-100">You agree to providing your data to Dpaul who may contact you.</label>
         </span>
-          <button onClick={handleClick} type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg w-full bg-blue-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+          <button id='btn__submit' onClick={handleClick} type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg w-full bg-blue-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
       </form>
   </div>
   <Footer />
--- mylink/src/components/Red.jsx
+++ mylink/src/components/Red.jsx
@@ -12,7 +12,7 @@ function Red() {
 
             <div className="flex justify-center">
             
-            <button id="id" type="button " className=" border-2 border-white bg-gray-200 mb-2  p-2 w-2/3 md:w-2/4 mt-6 hover:shadow-lg transform hover:scale-125 hover:font-bold hover:bg-gray-400  hover:border-b border-white  hover:text-white transition ease-out duration-300" > <Link to ="/Contact">Contact me</Link> </button>
+            <button id="contact" type="button " className=" border-2 border-white bg-gray-200 mb-2  p-2 w-2/3 md:w-2/4 mt-6 hover:shadow-lg transform hover:scale-125 hover:font-bold hover:bg-gray-400  hover:border-b border-white  hover:text-white transition ease-out duration-300" > <Link to ="/Contact">Contact me</Link> </button>
             </div>
             <Outlet />
             
