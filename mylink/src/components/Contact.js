58719d0410f83c54340448820f2256775633ed27
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-11-06 08:59:46 +0100

    fix the mobile responsiveness

--- mylink/src/components/Contact.js
+++ mylink/src/components/Contact.js
@@ -25,7 +25,7 @@ function Contact(){
       <h2 className="mb-4 text-4xl tracking-tight text-Inter text-left text-gray-900 dark:text-white">Contact me</h2>
       <p className="mb-8 lg:mb-16 font-light text-left text-gray-500 dark:text-gray-400 sm:text-xl">Hi there, contact me to ask me about anything you have in mind.</p>
       <form action="#" className="space-y-8">
-          <div className='grid gap-4 grid-cols-2'>
+          <div className='md:grid gap-4 grid-cols-2'>
                 <div className='w-full '>
               <label id='first_name' for="Firstname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First name</label>
               <input type="name" id="name" className="shadow-sm py-3 px-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Enter your first name" required />
