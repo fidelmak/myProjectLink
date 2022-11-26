2552de8c28ebedee4e34c1426d2c70af83dbe3ef
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-11-26 08:29:07 +0100

    new background color

--- mylink/tailwind.config.js
+++ mylink/tailwind.config.js
@@ -4,11 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        primary:'@FF6363',
+        primary:'#FF6363',
         secondary:{
           200:'#888883',
           100:'#E2E2D5'
-        }
+        },
+        main:"#fada5e"
       },
       fontFamily:{
         body:['Nunito']
