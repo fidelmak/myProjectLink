2abd4a219db28ab2679642969490127d5b8c19cc
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-27 23:11:15 +0100

    Update Nav.jsx

--- mylink/src/components/Nav.jsx
+++ mylink/src/components/Nav.jsx
@@ -17,13 +17,13 @@ function Nav(props) {
 
 <nav className="main">
 
-<p className="text-center text-white font-bold mr-12 " id='twitter'><i><a href="https://twitter.com/DelisIgib">@DelisIgib</a></i> </p> 
+<p className="text-center text-white font-bold mr-12 " id='twitter'><i><a href="https://twitter.com/DelisIgib"> @DelisIgib</a></i> </p> 
 
 
 <div className="mt-6">
     <span className="text-white  flex justify-center">
         
-       <p className=" border-2 p-2  border-gray-400 w-1/2 text-center ">Hey Ma chère! Je suis un développeur Web frontend qui aime concrétiser les conceptions </p> </span>
+       <p className=" border-2 p-2  border-gray-400 w-1/2 text-center ">Hey Ma chère! ♥️</p> </span>
     </div>
 
 </nav>
