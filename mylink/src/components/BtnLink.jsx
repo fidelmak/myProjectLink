f3500cc4e3fc361e1517a0a8695b9bba695c039e
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-04-28 19:38:16 +0100

    Update BtnLink.jsx

--- mylink/src/components/BtnLink.jsx
+++ mylink/src/components/BtnLink.jsx
@@ -5,19 +5,17 @@ import Button from './Button'
 function BtnLink() {
   return (
     <div>
-        <Button name="About me" link=" https://oluborodeakintunde.netlify.app/"/>
-         <Button name="LinkedIn me" link="https://www.linkedin.com/in/oluborode-akintunde-paul-8b3b2116a"/>
+          <Button name="LinkedIn Profile" link="https://www.linkedin.com/in/oluborode-akintunde-paul-8b3b2116a"/>
                            
 
   <Button name="Online programming platform " link="https://mainkey-tutorial.vercel.app/" id='btn__mainkeytut'/>
                     <Button name="Social Links "link="https://sociallincs.netlify.app/"/>
-                    <Button name="E-commerce website (Amazon-clone)" link="https://myamazon-clone.vercel.app/"/>
+                    
                     <Button name="Search your Favourite video" link="https://vidmax.vercel.app/"/>
                     <Button name="Get Palo whitelist (dApp) " link="https://get-palo-whitelist.vercel.app/"/>
                     <Button name="Tech-Talk"link="https://social-eosin-tau.vercel.app/#span" id='pitch'/>
                     <Button name="React Guidelines "link=" https://silly-davinci-6a83f1.netlify.app/#lifting_state_up" id='book__design'/>
                     
-                    <Button name="Just Me"link=" https://makekudi.netlify.app/" />
                     
     </div>
   )
